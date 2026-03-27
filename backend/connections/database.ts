2a0079328aa9248fea70892096de06814714e04a
aastharaut <aastharaut03@gmail.com>
2026-03-27 16:31:00 +0545

    setup sequelize for database.

---
+++ backend/connections/database.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+const sequelize = new Sequelize(
+  "postgres://postgres:1907@localhost:5432/realestate",
+  {
+    logging: false, //reducing console logs, set to true for debugging
+  },
+);
+
+// iife to test the connection and sync the models
+(async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+export default sequelize;
